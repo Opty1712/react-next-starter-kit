a65d1aec1f9b375b9d97a93d56d984d2398c9656
Andrey Edounov <opty@opty.ru>
2023-01-10 21:53:57 +0300

    feat: upgraded linaria

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,6 @@
-export const Component = () => <div data-testid="test">Hello</div>;
+import { styled } from 'linaria/react';
+export const Component = () => <Button data-testid="test">Hello</Button>;
+
+const Button = styled.button`
+  color: red;
+`;
