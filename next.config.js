b16024d55f06de1e35920a14fb8632982f36b174
Andrey Edounov <opty@opty.ru>
2023-01-10 21:26:33 +0300

    feat: upgraded package for the latest NodeJS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withImages = require('next-images');
 const withLinaria = require('next-linaria');
-const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([withLinaria, withImages]);
+module.exports = withLinaria({});
